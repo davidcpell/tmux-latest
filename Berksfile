89eafcffddfa55d751b4b7d1e0a534cdc4a9fbd5
David Pell <davidcpell@gmail.com>
2016-08-14 00:05:47 -0400

    no longer depend on community tmux ckbk

--- Berksfile
+++ Berksfile
@@ -1,5 +1,3 @@
 source 'https://supermarket.chef.io'
 
 metadata
-
-cookbook 'tmux', '~> 1.5.0'
--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,7 @@
-name 'ws-tmux'
+name 'tmux'
 maintainer 'David Pell'
 maintainer_email 'davidcpell@gmail.com'
 license 'all_rights'
 description 'Installs/Configures tmux'
 long_description 'Installs/Configures tmux'
 version '0.1.0'
-
-depends 'tmux', '~> 1.5.0'
-
