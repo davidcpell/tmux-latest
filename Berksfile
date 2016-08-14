d55af233b8e5d0bd7babf0b9f4975e8d2c8abc6d
David Pell <davidcpell@gmail.com>
2016-08-13 22:30:25 -0400

    bundle community tmux ckbk

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 source 'https://supermarket.chef.io'
 
 metadata
+
+cookbook 'tmux', '~> 1.5.0'
--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,10 @@
-name 'tmux'
+name 'ws-tmux'
 maintainer 'David Pell'
 maintainer_email 'davidcpell@gmail.com'
 license 'all_rights'
 description 'Installs/Configures tmux'
 long_description 'Installs/Configures tmux'
 version '0.1.0'
+
+depends 'tmux', '~> 1.5.0'
+
