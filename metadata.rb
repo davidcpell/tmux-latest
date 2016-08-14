ce3c9483ae051af740eb4d55507d17771ad1d04e
David Pell <davidcpell@gmail.com>
2016-08-13 22:28:55 -0400

    add metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'tmux'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'David Pell'
+maintainer_email 'davidcpell@gmail.com'
 license 'all_rights'
 description 'Installs/Configures tmux'
 long_description 'Installs/Configures tmux'
